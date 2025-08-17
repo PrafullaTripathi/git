49ea1966467da2ea0e1dd48b00fa7cab1cdcba06
prafulla <prafullakumar4321@gmail.com>
2025-08-18 01:33:47 +0530

    please add it in git

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+     subscription_id = "177b7e12-5f03-4f63-bcd1-ed6d1d776bff" 
+    }
+ 
+ resource "azurerm_resource_group" "gittest" {
+name = "gittest1"
+location = "eastus"
+ }
+
+
